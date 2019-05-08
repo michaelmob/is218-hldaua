40909a8edced8a298952c02f089c2b256e3f33af
thetarkus <mike@mikemob.com>
2019-05-07 21:31:17 -0400

    Add fetching all users and counting them test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -8,6 +8,33 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * Test if factory creates a User object.
+     *
+     * @return void
+     */
+    public function testFactoryCreatesUser()
+    {
+        $user = factory(\App\User::class)->make();
+
+        $this->assertInstanceOf(\App\User::class, $user);
+    }
+
+
+    /**
+     * Test if fetching all users returns all results.
+     *
+     * @return void
+     */
+    public function testUserFetchingReturnsAllResults()
+    {
+        $users = \App\User::all();
+        $userCount = \App\User::count();
+
+        $this->assertCount($userCount, $users);
+    }
     /**
      * A basic unit test example.
      *
